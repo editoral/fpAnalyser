76ac46f6b22efe0bbdd3c8f8de0e5ec5025e773a
MP <marc.porto@hotmail.com>
2016-01-14 20:10:25 +0100

    Minutia comparision

--- comp.py
+++ comp.py
@@ -1,7 +1,7 @@
 from imp import reload
 import numpy as np
 import re
-
+import math
 
 def act():
 	p = Parser()
@@ -9,21 +9,91 @@ def act():
 	return p
 
 def act2(firstFP):
+	np.set_printoptions(threshold=1000000)
 	a = FPAnalyzer()
-	a.compare(firstFP,1)	
+	a.compare(firstFP,1)
+	#return a.applyRotation([1,1],90)
 
 class FPAnalyzer:
 
 	def __init__(self):
 		self.p = Parser()
 		self.p.parseFile()
+		self.rotationAngle = 45
+		self.rotationSteps = 1
+		self.coordTreshold = 3
+		self.minuMatchTreshold = 5
+
+	def setValues(self, rotationAngle, rotationSteps, coordTreshold):
+		self.rotationAngle = rotationAngle
+		self.rotationSteps = rotationSteps
+		self.coordTreshold = coordTreshold
 
 
 	def compare(self,firstFP, secondFP):
 		matrix = self.buildVectorMatrix(firstFP)
 		matrix2 = self.buildVectorMatrix(secondFP)
+		self.comparision(matrix,matrix2)
+		#print(matrix)
 		
-	def evaluateTranslation(self, matrix, matrix2)
+
+	def comparision(self, matrix, matrix2):
+		mostMatches = 0
+		bestRotation = 0 
+		for rot in drange(0,self.rotationAngle,self.rotationSteps):
+			matches = []
+			counter = 0
+			for vector in matrix:
+				matches.extend(self.findCounterpart(vector, matrix2, rot, counter))
+				counter += 1
+			if len(matches) > mostMatches:
+				mostMatches = len(matches)
+				bestRotation = rot
+	
+	def findCounterpart(self, vector, matrix, rot, counter):
+		vector = self.applyRotation(vector, rot)
+		return self.checkForTranslatedEqual(vector, matrix, counter)
+
+	def checkForTranslatedEqual(self, vector, matrix, counter):
+		matches = []
+		counter2 = 0
+		for vector2 in matrix:
+			dx = round(vector[0]/vector2[0], 2)
+			dy = round(vector[1]/vector2[1], 2)
+			if dx == dy:
+				matches.append([counter, counter2])
+			counter2 += 1
+		return matches
+
+	def applyRotation(self, vec, rot):
+		rot = math.radians(rot)
+		cos = math.cos(rot)
+		sin = math.sin(rot)
+		rotationMatrix = np.matrix([[cos, -sin],[sin, cos]])
+		vectorMatrix = np.matrix([vec[0], vec[1]])
+		vectorMatrix = vectorMatrix.getT()
+		#vector = vectorMatrix.dot(rotationMatrix)
+		vector = rotationMatrix.dot(vectorMatrix)
+		vec[0] = vector[0]
+		vec[1] = vector[1]
+		return vec
+
+
+
+#	def evaluateRotation(self, matrix, matrix2):
+#		for vector in matrix:
+#			self.searchRotation(vector, matrix)
+#
+	#def evaluateTranslation(self, matrix, matrix2):
+		
+			
+	
+
+
+#	def searchRotation(self vector):
+
+
+	#def searchTranslatedCounterpart
 
 
 	def buildVectorMatrix(self,fpNumber):
@@ -44,8 +114,23 @@ class FPAnalyzer:
 				countj += 1
 			countk += 1
 			countj = 0
+		matrix = self.removeDuplicateVectors(matrix, countTot)
 		return matrix
 
+	def removeDuplicateVectors(self, matrix, length):	
+		result = np.zeros(((length/2),4))
+		counter = []
+		index = 0
+		for vector in matrix:
+			first = vector[2]
+			second = vector[3] 
+			valPair = [first, second]
+			valPair2 = [second, first]
+			if valPair not in counter and valPair2 not in counter:
+				counter.append(valPair)
+				result[index] = vector
+				index += 1
+		return result
 
 
 		
@@ -59,7 +144,7 @@ class Parser:
 	
 
 	def __init__(self):
-		self.f = open('C:/Users/Marc/SkyDrive/BFH2014/Biometrie/Kursaufgabe/Projekt/test.txt','r')
+		self.f = open('C:/Users/Marc/SkyDrive/BFH2014/Biometrie/Kursaufgabe/fpAnalyser/test.txt','r')
 		self.blockSize = 5
 		self.blockSizeCounter = 0
 		self.headers = []
@@ -148,3 +233,13 @@ class Minutiae:
 		if self.xCoord == min.xCoord and self.yCoord == min.yCoord:
 			result = True
 		return result
+
+
+
+
+#Hilfsfunktion für iteration über Dezimalwerte
+def drange(start, stop, step):
+	r = start
+	while r < stop:
+		yield r
+		r += step
